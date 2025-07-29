569d37aab7242ec42f72bce8ab99e34580851a50
teja-057 <tejaraghumandala1234@gmail.com>
2025-07-29 11:59:34 +0530

    updated the ps1

--- scripts/add-users-from-csv.ps1
+++ scripts/add-users-from-csv.ps1
@@ -9,34 +9,34 @@ if (-not (Test-Path $CsvPath)) {
     exit 1
 }
 
-# $auth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$Pat"))
-# $headers = @{ Authorization = "Basic $auth"; "Content-Type" = "application/json" }
+$auth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$Pat"))
+$headers = @{ Authorization = "Basic $auth"; "Content-Type" = "application/json" }
 
 $users = Import-Csv -Path $CsvPath
 
 foreach ($user in $users) {
     Write-Output $user
-    # $emailToAdd = $user.email
-    # $licenseType = $user.licensetype
+    $emailToAdd = $user.email
+    $licenseType = $user.licensetype
 
-    # Write-Output "Adding user: $emailToAdd with license type: $licenseType"
+    Write-Output "Adding user: $emailToAdd with license type: $licenseType"
 
-    # $postUrl = "https://vsaex.dev.azure.com/$Organization/_apis/userentitlements?api-version=7.1-preview.1"
+    $postUrl = "https://vsaex.dev.azure.com/$Organization/_apis/userentitlements?api-version=7.1-preview.1"
 
-    # $body = @{
-    #     accessLevel = @{
-    #         accountLicenseType = $licenseType
-    #     }
-    #     user = @{
-    #         principalName = $emailToAdd
-    #         subjectKind = "user"
-    #     }
-    # } | ConvertTo-Json -Depth 10
+    $body = @{
+        accessLevel = @{
+            accountLicenseType = $licenseType
+        }
+        user = @{
+            principalName = $emailToAdd
+            subjectKind = "user"
+        }
+    } | ConvertTo-Json -Depth 10
 
-    # try {
-    #     $response = Invoke-RestMethod -Uri $postUrl -Method Post -Headers $headers -Body $body
-    #     Write-Output "Successfully added: $emailToAdd"
-    # } catch {
-    #     Write-Error "Failed to add user "
-    # }
+    try {
+        Invoke-RestMethod -Uri $postUrl -Method Post -Headers $headers -Body $body
+        Write-Output "Successfully added: $emailToAdd"
+    } catch {
+        Write-Error "Failed to add user "
+    }
 }
